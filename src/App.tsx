e6436df74e6650f58a07186942eb3b85ab5b166d
Allie Bourden <alexandreabourden@gmail.com>
2024-05-18 00:33:08 -0230

    build - added pages to navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Farmers from './pages/Farmers';
+import Marketplace from './pages/Marketplace';
 
 const App: React.FC = () => {
   return (
@@ -9,6 +11,8 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={ <Home />} />
         <Route path="/about" element={ <About />} />
+        <Route path="/farmers" element={ <Farmers />} />
+        <Route path="/marketplace" element={ <Marketplace />} />
       </Routes>
     </Router>
   );
