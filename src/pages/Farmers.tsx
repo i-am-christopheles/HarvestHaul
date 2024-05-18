94e482585c0019dd2bb45b97ddcb12991f7c8fa4
Chris Smith <chris.smith@verafin.com>
2024-05-18 11:13:52 -0230

    add fetch profiles to Farmers.tsx

--- src/pages/Farmers.tsx
+++ src/pages/Farmers.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import NavBar from '../components/NavBar';
+import {Schema} from "../../amplify/data/resource.ts";
+import {generateClient} from "aws-amplify/src/api";
+
+const client = generateClient<Schema>();
 
 const Farmers: React.FC = () => {
+    const [profiles, setProfiles] = useState<Schema["Producer"]["type"][]>([]);
+    const fetchProfiles = async () => {
+        const { data: items, errors } = await client.models.Producer.list();
+        console.log(errors);
+        setProfiles(items);
+    };
+
+    fetchProfiles();
   return (
     <>
       <NavBar />
       <h1>Farmers</h1>
+        <div>
+            <ul>
+                {profiles.map(({ id, name, region }) => (
+                    <>
+                        <li key={id}>Farm name: {name}</li>
+                        <li key={id}>Region: {region}</li>
+                    </>
+                ))}
+            </ul>
+        </div>
     </>
   );
 }
