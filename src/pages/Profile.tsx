a1724d2ae452f82af74de3b453e62c85c34811a0
Chris Smith <chris.smith@verafin.com>
2024-05-18 10:49:55 -0230

    add fetch profile to Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import NavBar from '../components/NavBar';
+import type { Schema } from '../../amplify/data/resource';
+import {generateClient} from "aws-amplify/data";
+
+const client = generateClient<Schema>();
+
 
 const Profile: React.FC = () => {
-  return (
+    const [profiles, setProfiles] = useState<Schema["Producer"]["type"][]>([]);
+    const fetchProfiles = async () => {
+        const { data: items, errors } = await client.models.Producer.list();
+        console.log(errors);
+        setProfiles(items);
+    };
+
+    useEffect(() => {
+        fetchProfiles();
+    }, []);
+
+    const createProfile = async () => {
+        await client.models.Producer.create({
+            name: window.prompt("Name?"),
+            region: window.prompt("Region?"),
+        });
+
+        fetchProfiles();
+    }
+    return (
     <>
       <NavBar />
+        <div>
+            <button onClick={createProfile}>Add new Profile</button>
+            <ul>
+                {profiles.map(({ id, name }) => (
+                    <li key={id}>{name}</li>
+                ))}
+            </ul>
+        </div>
     </>
   );
 }
