c92f0a448b35a039e93d6e3a6ba082322de3cd44
Allie Bourden <alexandreabourden@gmail.com>
2024-05-17 23:57:00 -0230

    build - installed chakra ui, wrapped /App

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+import { ChakraProvider } from "@chakra-ui/react";
 
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
