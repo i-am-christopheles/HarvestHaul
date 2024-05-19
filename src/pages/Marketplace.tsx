7ff3d0cb4a379da0907e76e844472c18e1670382
Chris Smith <chris.smith@verafin.com>
2024-05-19 01:03:18 -0230

    remove chatbot component

--- src/pages/Marketplace.tsx
+++ src/pages/Marketplace.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
-import ChatContainer from '../components/ChatContainer';
 
 const Marketplace: React.FC = () => {
   return (
     <>
       <NavBar />
-      <ChatContainer />
       <h1>Marketplace</h1>
     </>
   );
